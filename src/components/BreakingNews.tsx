ea0062d800a62a93ee0002f4fa844c27cb5f6260
Patrick Rose <pjr0911025@googlemail.com>
2022-01-17 13:29:14 +0000

    Clean up animations and positioning

--- src/components/BreakingNews.tsx
+++ src/components/BreakingNews.tsx
@@ -10,7 +10,7 @@ export default function BreakingNews({content}: BreakingNewsProps) {
             <footer className="bg-red-600 text-white mt-4">
                 <div className="w-100 px-2 flex flex-col h-24 overflow-hidden">
                     <h3 className="text-2xl pb-2">Breaking news</h3>
-                    <p className="text-xl py-2 animate-marquee">{content}</p>
+                    <p className="text-xl py-2 animate-marquee lg:[animation-duration:15s] xl:[animation-duration:20s]">{content}</p>
                 </div>
             </footer>
         );
--- src/components/Roles.tsx
+++ src/components/Roles.tsx
@@ -10,7 +10,7 @@ export default function Roles(props: RoleProps) {
   const { phaseNumber } = props;
 
   return (
-    <div className="container roles">
+    <div className="container text-3xl flex-1 flex items-center">
       <ul className="list-unstyled">
         {
             AllRoles.map(
